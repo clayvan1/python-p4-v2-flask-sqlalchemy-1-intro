cb7bc0c41bea7598edbcf3ae65c30745edb4dfb6
Clayvan Omondi <claydrinch@gmail.com>
2025-05-12 10:10:00 -0700

    new

--- server/app.py
+++ server/app.py
@@ -1,17 +1,14 @@
 # server/app.py
-
 from flask import Flask
 from flask_migrate import Migrate
-
 from models import db
 
-# create a Flask application instance
+# create a Flask application object
 app = Flask(__name__)
 
-# configure the database connection to the local file app.db
+# configure a database connection to the local file app.db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# configure flag to disable modification tracking and use less memory
+# disable modification tracking to use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # create a Migrate object to manage schema modifications
@@ -20,6 +17,22 @@ migrate = Migrate(app, db)
 # initialize the Flask application to use the database
 db.init_app(app)
 
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+# server/models.py
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+# contains definitions of tables and associated schema constructs
+# read more about Metadata using the link at the bottom of the page
+metadata = MetaData()
+# create the Flask SQLAlchemy extension
+db = SQLAlchemy(metadata=metadata)
+
+# define a model class by inheriting from db.Model.
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
